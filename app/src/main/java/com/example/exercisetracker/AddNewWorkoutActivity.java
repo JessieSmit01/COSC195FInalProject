5deae543ae381ffdc055c0dab48ea04712cb85b2
Jessie Smith <Jessie.smit01@gmail.com>
2019-05-26 11:43:08 -0600

    Created add new workout layout -Jessie Smith

---
+++ app/src/main/java/com/example/exercisetracker/AddNewWorkoutActivity.java
@@ -0,0 +1,19 @@
+package com.example.exercisetracker;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AddNewWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_workout);
+
+    }
+
+}
--- app/src/main/java/com/example/exercisetracker/MainActivity.java
+++ app/src/main/java/com/example/exercisetracker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.exercisetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btnAdd:
+                Intent intent = new Intent(this, AddNewWorkoutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnView:
+
+                break;
+        }
     }
 }
